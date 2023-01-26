37ef7f252985f03c318e5f8d6a4389a3445ba577
AlbertoArochal <arochaldev@gmail.com>
2023-01-26 23:20:04 +0100

    added test to signwithgoogle

--- src/components/helpers/signwithgoogle.test.tsx
+++ src/components/helpers/signwithgoogle.test.tsx
@@ -1,38 +1,31 @@
-import { signInWithGoogle } from './signwithgoogle';
 import { auth, Firedb } from '../firebase/firebase';
 import { signInWithPopup } from 'firebase/auth';
 import { provider } from '../firebase/firebase';
 import { set, ref } from 'firebase/database';
+
+import { signInWithGoogle } from './signwithgoogle';
+
+jest.mock('firebase/auth');
+jest.mock('firebase/database');
+
 describe('signInWithGoogle', () => {
-    it('should call signInWithPopup and set user data in the database', () => {
-        const mockSignInWithPopup = jest.fn();
-        const mockSet = jest.fn();
-        const mockRef = jest.fn();
-
-        jest.mock('firebase', () => ({
-            auth: {
-                signInWithPopup: mockSignInWithPopup,
-            },
-            database: () => ({
-                ref: mockRef,
-                set: mockSet,
-            }),
-        }));
-
-        const userData = {
+    it('should sign in with google', async () => {
+        const mockUser = {
             uid: '123',
-            name: 'John Doe',
-            email: 'johndoe@example.com',
-            profilePic: 'https://example.com/profilepic.jpg',
-            decks: { deck1: ['necropotency'] },
+            displayName: 'Alberto',
+            decks: {},
+            name: 'Alberto',
+            profilePic: 'https://picsum.photos/200',
         };
-        const result = { user: userData };
-        mockSignInWithPopup.mockResolvedValue(result);
-
-        signInWithGoogle();
-
-        expect(mockSignInWithPopup).toHaveBeenCalledWith(auth, provider);
-        expect(mockRef).toHaveBeenCalledWith(Firedb, 'users/123');
-        expect(mockSet).toHaveBeenCalledWith(userData);
+        const mockUserRef = {
+            set: jest.fn(),
+        };
+        (signInWithPopup as jest.Mock).mockResolvedValue({
+            user: mockUser,
+        });
+        (ref as jest.Mock).mockReturnValue(mockUserRef);
+        await signInWithGoogle();
+        expect(signInWithPopup).toHaveBeenCalledWith(auth, provider);
+        expect(set).toHaveBeenCalledTimes(1);
     });
 });
