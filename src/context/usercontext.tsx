0eb7f6f8c7d53af9da506b2955cbe2c9bbb11d3a
AlbertoArochal <arochaldev@gmail.com>
2023-01-27 08:38:43 +0100

    little fix on context

--- src/context/usercontext.tsx
+++ src/context/usercontext.tsx
@@ -3,7 +3,9 @@ import { pwReducer, initialState } from '../components/reducers/pwreducer';
 import { pwType } from '../models/pwtype';
 export const userContext = createContext({
     user: initialState.user,
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
     setUser: (user: pwType) => {},
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
     logout: () => {},
 });
 
